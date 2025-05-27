9420ce0650050ca317ce4c00279371742cfb097f
Dominik Sumer <domi.sumer@gmail.com>
2025-05-27 15:48:21 +0200

    export vemetric object for the astro lib

--- packages/astro/src/component.ts
+++ packages/astro/src/component.ts
@@ -1,4 +1,5 @@
-// @ts-expect-error we need to export the astro component
 // eslint-disable-next-line no-useless-rename
 export { default as default } from './index.astro';
 export type { Options } from './vemetric.d.ts';
+// @ts-expect-error we export the vemetric object from the dist folder
+export { vemetric } from './vemetric.mjs';
